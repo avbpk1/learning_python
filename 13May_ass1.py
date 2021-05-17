3f85a75657077941f7299207c3ead925b3d3d004
avbpk1 <63863374+avbpk1@users.noreply.github.com>
2021-05-17 03:12:39 +0100

    Update 13May_ass1.py

--- 13May_ass1.py
+++ 13May_ass1.py
@@ -10,3 +10,16 @@ odd_list = [n2 for n2 in num_list if n2%2 != 0]
 print(sorted(even_list))
 print(sorted(odd_list))
 # print(f" Sorted order of even numbers : {sorted(even_list)}  \n Sorted order for odd numbers : {sorted(odd_list)}")
+
+# Accept name and multi phone numbers
+contacts = {}
+str = input("Enter Name and Phone number separated by comma [type end to enter input]: ")
+while str != 'end':
+    name,mobile=str.split(',')
+    if name in contacts :
+        if mobile not in contacts.values():
+            contacts[name] += (',' + mobile)
+    else:
+        contacts[name] = (mobile)
+    str = input("Enter Name and Phone number separated by comma [type end to enter input]: ")
+print(contacts)
